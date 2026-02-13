67abd5d0f0bc06076c9bec24edd4eaafba402d1d
diymail01 <diymail01@gmail.com>
2026-02-13 21:28:57 +0530

    bkwas

--- home/batman/git.nix
+++ home/batman/git.nix
@@ -4,4 +4,14 @@
     userName = "diymail01";
     userEmail = "diymail01@gmail.com";
   };
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+    };
+  };
 }
