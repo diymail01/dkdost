2e1345634a71c8ca8b1dcafad69f700c8c7074b2
diymail01 <diymail01@gmail.com>
2026-02-14 13:57:53 +0530

    direnv

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "user0 NixOS system";
+  description = "user0 abyss";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
@@ -8,26 +8,58 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = { self, nixpkgs, home-manager }:
+  outputs = { self, nixpkgs, home-manager, sops-nix }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in
   {
+    # =========================
+    # NixOS Configuration
+    # =========================
     nixosConfigurations.user0 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
 
-modules = [
- ./hosts/user0/configuration.nix
-  home-manager.nixosModules.home-manager
-  {
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.backupFileExtension = "bak";
-    home-manager.users.batman = import ./home/batman;
-    home-manager.users.ironman = import ./home/ironman.nix;
-    home-manager.users.omniman = import ./home/omniman.nix;
-  }
-];
+      modules = [
+        ./hosts/user0/configuration.nix
+
+        # Secrets layer
+        sops-nix.nixosModules.sops
+
+        # Home Manager
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "bak";
+
+          home-manager.users.batman = import ./home/batman;
+          home-manager.users.ironman = import ./home/ironman.nix;
+          home-manager.users.omniman = import ./home/omniman.nix;
+        }
+      ];
+    };
+
+    # =========================
+    # Dev Shells
+    # =========================
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        git
+        nodejs
+        python3
+        go
+        rustc
+        cargo
+      ];
+
+      shellHook = ''
+        echo "Dev shell activated"
+      '';
     };
   };
 }
-
